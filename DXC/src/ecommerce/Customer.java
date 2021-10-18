a9f099d289dcb5cab1f727736a43b3bbd872e96a
ManishMarvel <manishsesetty@gmail.com>
2021-10-18 16:55:07 +0530

    initial commit

---
+++ DXC/src/ecommerce/Customer.java
@@ -0,0 +1,15 @@
+package ecommerce;
+
+public class Customer {
+ public void walkin() {
+	 System.out.println("abdul is going inside big bazar");
+	 
+ }
+ public void shop() {
+	 System.out.println("customer is shopping");
+ }
+ public void checkout() {
+	 Cashier ansariCashier=new Cashier();
+	 ansariCashier.estimate();
+ }
+}
