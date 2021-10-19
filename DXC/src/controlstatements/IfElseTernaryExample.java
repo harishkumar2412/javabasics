28f4c885327528a7cded91a743bc346884a6de9a
ManishMarvel <manishsesetty@gmail.com>
2021-10-19 15:06:35 +0530

    DebugDemo

---
+++ DXC/src/controlstatements/IfElseTernaryExample.java
@@ -0,0 +1,10 @@
+package controlstatements;
+
+public class IfElseTernaryExample {
+	public static void main(String[] args) {
+		int  number = 24;
+		String output = (number % 2 == 0)?"even number":"odd number";
+		System.out.println(output);
+	}
+
+}
