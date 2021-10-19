83d71a2fe30d11ac339e1cb435150256be30523a
HarishKumar <sesettyharish@gmail.com>
2021-10-19 12:54:09 +0530

    controlstatements

---
+++ DXC/src/controlstatements/IfElseExample.java
@@ -0,0 +1,14 @@
+package controlstatements;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		int number = 24;
+		if(number % 2 == 0) {
+			System.out.println("even number");
+		}
+		else {
+			System.out.println("odd number");
+		}
+	}
+
+}
