7de7897f551bd3a94f3add6531972e4f48f894cb
HarishKumar <sesettyharish@gmail.com>
2021-10-20 11:58:06 +0530

    static

---
+++ DXC/src/objectclass/Util.java
@@ -0,0 +1,20 @@
+package objectclass;
+
+public class Util {
+
+	/**
+	 * this class has all utility methods
+	 * @author Admin
+	 *
+	 */
+
+		
+		public static String COMPANY_NAME = "dxc";
+		
+		public static int incrementByTen(int no) {
+			return no + 10;
+		}
+
+
+
+}
