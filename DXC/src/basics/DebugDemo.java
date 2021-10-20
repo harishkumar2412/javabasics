0f45464ab24cabae665515e69a9bcb915c2b5ef1
HarishKumar <sesettyharish@gmail.com>
2021-10-20 11:52:59 +0530

    Static

--- DXC/src/basics/DebugDemo.java
+++ DXC/src/basics/DebugDemo.java
@@ -7,17 +7,13 @@ public class DebugDemo {
 		Calculator calc = new Calculator();
 		int sum = calc.add(10, 20);
 		System.out.println("the sum is--"+sum);
-		Student abdul = new Student("ansari",123);
-		Student myStudent = new Student();
-		Student yourStudent = new Student("geethanjali");
+		
 		for(int a=0; a<5; a++) {
-			int j = a *2;
-			int f = a+ 5 *3 +j;
-			System.out.println("the no is="+f);
+			int f = a+ 5 *3;
+			//System.out.println("the no is="+f);
 			
 		}
-		
-
+		//throw new NullPointerException();
 	}
 
 }
---
+++ DXC/src/objectclass/Calcul.java
@@ -0,0 +1,14 @@
+package objectclass;
+
+public class Calcul {
+	
+		  static int cube(int x){  
+		  return x*x*x;  
+		  }  
+		  
+		  public static void main(String args[]){  
+		  int result=Calcul.cube(5);  
+		  System.out.println(result);  
+		  }  
+		
+}
