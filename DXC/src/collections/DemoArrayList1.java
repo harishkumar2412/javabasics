a5d56a7db582deea239554fe9b67da957003c4c1
HarishKumar <sesettyharish@gmail.com>
2021-10-26 12:21:11 +0530

    tree

---
+++ DXC/src/collections/DemoArrayList1.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArrayList1 {
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("harish");
+		names.add("kumar");
+		System.out.println(names);
+	}
+
+}
