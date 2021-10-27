04ac3bb412c65158132022e84b8729c85567d9c6
HarishKumar <sesettyharish@gmail.com>
2021-10-27 15:43:26 +0530

    collection and unit test

--- DXC/src/abstraction/BmwFlyingCar.java
+++ DXC/src/abstraction/BmwFlyingCar.java
@@ -2,19 +2,32 @@ package abstraction;
 
 public class BmwFlyingCar extends FlyingCar {
 
+
+	@Override
 	public void stopCar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void flyCar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
 	public void landCar() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public static	int add(int a, int b) {
+		return a+b;
+	}
+
+	public static boolean isEligibletoVote(int age) {
+		if (age >17) {return true;}
+		return false;
 	}
 
 
