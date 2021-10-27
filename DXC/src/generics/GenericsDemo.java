7ce237b874a6ed0b0f7c0b45c368fde0fe534fd6
HarishKumar <sesettyharish@gmail.com>
2021-10-27 17:03:46 +0530

    generics --bounded types

--- DXC/src/generics/GenericsDemo.java
+++ DXC/src/generics/GenericsDemo.java
@@ -2,14 +2,20 @@ package generics;
 
 public class GenericsDemo {
 	public static void main(String[] args) {
-		Account<String> account = new Account<String>();
-		account.setobj("sbi account");
-		System.out.println(account.getobj());
-		
+		/*
+		 * Account<String> account = new Account<String>();
+		 * account.setObj("sbi account"); System.out.println(account.getObj());
+		 */
+
 		Account<Integer> gpayAccount = new Account<>();
 		gpayAccount.setobj(984569876);
 		System.out.println("gpay account no is"+ gpayAccount.getobj());
+
+		Account <Float> phonePeAccount = new Account<>();
+		phonePeAccount.setobj(3.14f);
+		System.out.println("phonepay account no is"+ phonePeAccount.getobj());
+		}
 		
-	}
+	
 
 }
