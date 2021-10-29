a80ed9efc6a2f37a7a151a812f360c94645caca8
HarishKumar <sesettyharish@gmail.com>
2021-10-29 12:53:45 +0530

    seperate package for factory classes

--- DXC/src/dependency/injection/FrameworkFactory.java
+++ DXC/src/dependency/injection/FrameworkFactory.java
@@ -1,10 +1,25 @@
 package dependency.injection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import dependency.injection.factory.ServiceFactory;
+
+
 public class FrameworkFactory {
-	public static void main(String[] args) {
-		Service serviceb = new
+	public static void main(String[] args) throws IOException {
+		ServiceFactory serviceFactory = new ServiceFactory();
+		System.out.print("Enter the name of plan for which the bill will be generated: ");  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+		String serviceName = br.readLine();  
+
+		Service service  = serviceFactory.getService(serviceName);  
+
+		
+		Service serviceb = service;
 				//AirtelService();
-				ServiceB();
+				//ServiceB();
 		Client client = new ClientA(serviceb); //constructor injection
 		client.doSomething();
 	}
---
+++ DXC/src/dependency/injection/factory/ServiceB.java
@@ -0,0 +1,13 @@
+package dependency.injection.factory;
+
+import dependency.injection.Service;
+
+public class ServiceB  implements Service{
+
+	@Override
+	public String getInfo() {
+        return "ServiceB’s Info";
+	}
+	
+	
+}
