ae58a94d0e670bc0233c5b601860421f9b1cb8e5
HarishKumar <sesettyharish@gmail.com>
2021-11-08 17:13:24 +0530

    call backs

---
+++ DXC/src/callbacks/Me.java
@@ -0,0 +1,11 @@
+package callbacks;
+
+public class Me {
+	
+	public static void main(String[] args) {
+		OfficeBoy ramu = new OfficeBoy();
+		MobilePhone mobilePhone = new MyAccountant();
+		ramu.doGovtWork(mobilePhone);
+	}
+
+}
---
+++ DXC/src/callbacks/MobilePhone.java
@@ -0,0 +1,9 @@
+package callbacks;
+
+public interface MobilePhone {
+	
+	void successGreenButton();
+	void failureRedButton();
+	
+
+}
